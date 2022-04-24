aac8be38e602ca62aacbd4a4894912a95813bc49
Ravi Dubey <38419795+ravi0dubey@users.noreply.github.com>
2022-04-24 19:29:00 +0000

    Final Changes

--- Blackjack (copy).py
+++ Blackjack (copy).py
@@ -18,72 +18,60 @@ def get_your_score():
   your_score = generate_score()
   print(f"your_score: {your_score}" )
   your_hand.append(your_score)
-  your_score = cal_score(your_hand)
+  your_score = cal_your_score()
   print(f"your cards{your_hand},current score: {your_score}")
 
 def get_computer_score():
   computer_score = generate_score()
   print(f"computer_score: {computer_score}" )
   computer_hand.append(computer_score)
-  computer_score= cal_score(computer_hand)
+  computer_score= cal_computer_score()
   print(f"Computer's card : {computer_hand},current score: {computer_score}")
 
-def compare_score(your_score, computer_score):
-  if your_score == computer_score:
-    return "Draw :)"
-  elif computer_score == 0:
-    return "Computer Win as Computer has BlackJack"
-  elif your_score == 0:
-    return "You Win as you have a BlackJack"
-  elif your_score > computer_score:
-    return "You Win!!!"
-  else:
-    return "Computer Win!!! "
 
-def cal_score(cards):
-  if sum(cards) == 21 and len(cards) == 2:
-    return 0
-  if sum(cards) > 21 and 11 in cards:
-     cards.remove(11)
-     cards.append(1)   
-    
-  return sum(cards)
+def cal_your_score():
+  total_your_score = 0
+  for number in your_hand:   
+    total_your_score += number
+  return total_your_score
 
+def cal_computer_score():
+  total_computer_score = 0
+  for number in computer_hand:
+    total_computer_score += number
+  return total_computer_score
 
 play_game = input("Do you want to play a game of BlackJack? type 'y' or 'n': ")
 
 while  play_game != 'n':     
   get_your_score()
   get_computer_score()
-  your_score = cal_score(your_hand)
-  computer_score= cal_score(computer_hand)
-  
-  if (your_score == 0 or your_score > 21):
+  your_score = cal_your_score()
+  computer_score= cal_computer_score()
+  if your_score > 21:
      print("Your score greater than 21.You loose!Game Over!!!")
      break
   
-  if computer_score > 21 or computer_score == 0:
+  if computer_score > 21:
      print("Computer Score greater than 21. You Win Computer Loose!Game Over!!!")
      break
   
   proceed_game = input("Type 'y' to get another card, type 'n' to pass:")
   
-  if computer_score != 0 and proceed_game == 'n':
-    while computer_score < 17:
-      get_computer_score()
-      computer_score= cal_score(computer_hand) 
-    
-    your_score = cal_score(your_hand)
+  if proceed_game == 'n':
+    your_score = cal_your_score()
     print(f"Your final hand :{your_hand}, your final_score:{your_score}" )
+    computer_score= cal_computer_score()
     print(f"Computer's final hand :{computer_hand},final_score:{computer_score}")
   
-    print(compare_score(your_score,computer_score))
-    
+    if (your_score >= computer_score):
+      print("You win :)")
+    else:
+      print("Computer win :(")
     play_game = input("Do you want to play a game of BlackJack? type 'y' or 'n': ")
     your_hand = []
     computer_hand= []
 
  
    
-
 
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
-cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
 
 def generate_score():
-   rand_number = random.randint(0,12)
-   print(f"rand_number: {rand_number}")
-   return cards[rand_number]
+  cards = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+  rand_number = random.choice(cards)
+  print(f"rand_number: {rand_number}")
+  return rand_number
 
 play_game = 'y'
 
@@ -18,60 +18,72 @@ def get_your_score():
   your_score = generate_score()
   print(f"your_score: {your_score}" )
   your_hand.append(your_score)
-  your_score = cal_your_score()
+  your_score = cal_score(your_hand)
   print(f"your cards{your_hand},current score: {your_score}")
 
 def get_computer_score():
   computer_score = generate_score()
   print(f"computer_score: {computer_score}" )
   computer_hand.append(computer_score)
-  computer_score= cal_computer_score()
+  computer_score= cal_score(computer_hand)
   print(f"Computer's card : {computer_hand},current score: {computer_score}")
 
+def compare_score(your_score, computer_score):
+  if your_score == computer_score:
+    return "Draw :)"
+  elif computer_score == 0:
+    return "Computer Win as Computer has BlackJack"
+  elif your_score == 0:
+    return "You Win as you have a BlackJack"
+  elif your_score > computer_score:
+    return "You Win!!!"
+  else:
+    return "Computer Win!!! "
 
-def cal_your_score():
-  total_your_score = 0
-  for number in your_hand:   
-    total_your_score += number
-  return total_your_score
+def cal_score(cards):
+  if sum(cards) == 21 and len(cards) == 2:
+    return 0
+  if sum(cards) > 21 and 11 in cards:
+     cards.remove(11)
+     cards.append(1)   
+    
+  return sum(cards)
 
-def cal_computer_score():
-  total_computer_score = 0
-  for number in computer_hand:
-    total_computer_score += number
-  return total_computer_score
 
 play_game = input("Do you want to play a game of BlackJack? type 'y' or 'n': ")
 
 while  play_game != 'n':     
   get_your_score()
   get_computer_score()
-  your_score = cal_your_score()
-  computer_score= cal_computer_score()
-  if your_score > 21:
+  your_score = cal_score(your_hand)
+  computer_score= cal_score(computer_hand)
+  
+  if (your_score == 0 or your_score > 21):
      print("Your score greater than 21.You loose!Game Over!!!")
      break
   
-  if computer_score > 21:
+  if computer_score > 21 or computer_score == 0:
      print("Computer Score greater than 21. You Win Computer Loose!Game Over!!!")
      break
   
   proceed_game = input("Type 'y' to get another card, type 'n' to pass:")
   
-  if proceed_game == 'n':
-    your_score = cal_your_score()
+  if computer_score != 0 and proceed_game == 'n':
+    while computer_score < 17:
+      get_computer_score()
+      computer_score= cal_score(computer_hand) 
+    
+    your_score = cal_score(your_hand)
     print(f"Your final hand :{your_hand}, your final_score:{your_score}" )
-    computer_score= cal_computer_score()
     print(f"Computer's final hand :{computer_hand},final_score:{computer_score}")
   
-    if (your_score >= computer_score):
-      print("You win :)")
-    else:
-      print("Computer win :(")
+    print(compare_score(your_score,computer_score))
+    
     play_game = input("Do you want to play a game of BlackJack? type 'y' or 'n': ")
     your_hand = []
     computer_hand= []
 
  
    
+
 
